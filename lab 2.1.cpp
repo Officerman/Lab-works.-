824d8d6e919fcc1b2a974b1d94816b2d0697b75c
1002 <146112815+Officerman@users.noreply.github.com>
2023-10-25 16:34:01 +0300

    lab 2

---
+++ lab 2.1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* prev;
+    Node* next;
+
+    Node(int val) : data(val), prev(nullptr), next(nullptr) {}
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    DoubleLinkedList() : head(nullptr), tail(nullptr) {}
+
+    void insertNode(int val) {
+        Node* newNode = new Node(val);
+
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    void outList() {
+        Node* current = head;
+        while (current != nullptr) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    DoublyLinkedList myList;
+    myList.insertNode(1);
+    myList.insertNode(2);
+    myList.insertNode(3);
+
+    myList.outList();
+
+    return 0;
+}
